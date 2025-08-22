511791d732c1ef8168eb02843cf7bf3383b11b08
Shubhambhoir883 <bhoirshubham883@gmail.com>
2025-08-22 18:55:23 +0530

    react initial commit

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  js.configs.recommended,
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      // ✅ Run prettier as an ESLint rule
+      "prettier/prettier": "error",
+
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-undef": "error",
+      "no-tabs": ["error", { allowIndentationTabs: true }],
+      "max-len": ["error", { code: 200, tabWidth: 2, ignoreUrls: true }],
+      indent: ["error", "tab", { SwitchCase: 1 }],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "no-use-before-define": "error",
+      "no-console": "error",
+    },
+  },
+]);
---
+++ lint-staged.config.js
@@ -0,0 +1,3 @@
+export default {
+	"*.{js,jsx}": ["eslint --fix", "prettier --write"],
+};
